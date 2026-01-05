fd04b5c6a21368e2f7f4463e1b1a6108d1f48740
realfatihciftci <fatih.ciftci@std.bogazici.edu.tr>
2026-01-05 21:38:27 +0300

    Dashboard Complete

---
+++ AddTransactions.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinansCepte.Pages;
+
+public partial class AddTransactions : ContentPage
+{
+    public AddTransactions()
+    {
+        
+    }
+}
