425ef48901aa0a13334e5b750954792725eb6b53
realfatihciftci <fatih.ciftci@std.bogazici.edu.tr>
2026-01-05 20:26:59 +0300

    Start

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "FinansCepte")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "FinansCepte.Pages")]
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace FinansCepte;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
