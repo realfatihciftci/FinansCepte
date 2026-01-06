621c18c8da4d58199bff17d706a80c8165a3ead6
realfatihciftci <fatih.ciftci@std.bogazici.edu.tr>
2026-01-06 22:57:16 +0300

    marketitem and transaction classları

---
+++ Models/MarketItem.cs
@@ -0,0 +1,8 @@
+namespace FinansCepte.Models;
+
+public class MarketItem
+{
+    public string Name { get; set; }
+    public string BuyPrice {get; set;}
+    public string SellPrice {get; set;}
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,15 @@
+namespace FinansCepte.Models;
+
+public class Transaction
+{
+    public int Id {get; set;}
+    public string Type {get; set;}
+    public string Title{get; set;}
+    public decimal Amount {get; set;}
+    public DateTime Date {get; set;}
+    
+    public string AssetName {get; set;}
+    public double Quantity {get; set;}
+    public decimal UnitPrice {get; set;}
+    
+}
