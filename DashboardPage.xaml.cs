55a3a9c14ce400e8b22229c26966d33cc93ec99e
realfatihciftci <fatih.ciftci@std.bogazici.edu.tr>
2026-01-06 22:56:35 +0300

    Dashboard Page update with fakeDb

--- DashboardPage.xaml.cs
+++ DashboardPage.xaml.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FinansCepte.Data;
+using FinansCepte.Models;
+using System.Linq;
 
 namespace FinansCepte;
 
@@ -11,5 +14,36 @@ public partial class DashboardPage : ContentPage
     public DashboardPage()
     {
         InitializeComponent();
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        LoadData();
+    }
+
+    void LoadData()
+    {
+        var transactions = FakeDb.Transactions;
+        
+        decimal totalIncome = transactions
+            .Where((t => t.Type == "Gelir"))
+            .Sum(t => t.Amount);
+        
+        decimal totalExpense = transactions
+            .Where(t => t.Type == "Gider")
+            .Sum(t => t.Amount);
+        
+        decimal balance = totalIncome - totalExpense;
+
+        LblTotalIncome.Text = totalIncome.ToString("N2") + "₺";
+        LblTotalExpense.Text = totalExpense.ToString("N2") + "₺";
+        LblTotalBalance.Text = balance.ToString("N2") + "₺";
+
+        if (balance < 0)
+            LblTotalBalance.TextColor = Colors.Red;
+        else
+            LblTotalBalance.TextColor = Colors.Green;
+
     }
 }
